a76bababf1ff4ef50c32252b088b45886e63be55
anshu <anshu.iiita.10@gmail.com>
2018-10-12 15:32:45 -0500

    initial commit of full repository

---
+++ cmake-build-debug/CMakeFiles/os_hw2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_hw2.dir/main.c.o"
+  "os_hw2.pdb"
+  "os_hw2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/os_hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/os_hw2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for os_hw2.
+# This may be replaced when dependencies are built.
