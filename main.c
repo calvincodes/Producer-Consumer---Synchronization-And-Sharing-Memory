6f712db8f75ef6e38bb546af49213419d9cbe189
calvincodes <arpitjain1821@gmail.com>
2018-10-15 07:40:34 -0500

    Adding reader module

--- main.c
+++ main.c
@@ -6,45 +6,45 @@
 #include "munch2.h"
 #include "writer.h"
 
-int main() {
-    pthread_t reader;
-    pthread_t munch1;
-    pthread_t munch2;
-    pthread_t writer;
-    printf("Hello, World!\n");
-    Queue *queue1 = CreateStringQueue(10);
-    Queue *queue2 = CreateStringQueue(10);
-    Queue *queue3 = CreateStringQueue(10);
-//    if(pthread_create(&reader, NULL, readInput, queue1) != 0){
-//        printf("Thread Creation Failed for reader");
+//int main_() {
+//    pthread_t reader;
+//    pthread_t munch1;
+//    pthread_t munch2;
+//    pthread_t writer;
+//    printf("Hello, World!\n");
+//    Queue *queue1 = CreateStringQueue(10);
+//    Queue *queue2 = CreateStringQueue(10);
+//    Queue *queue3 = CreateStringQueue(10);
+////    if(pthread_create(&reader, NULL, readInput, queue1) != 0){
+////        printf("Thread Creation Failed for reader");
+////        return 0;
+////    }
+////    if(pthread_create(&munch1, NULL, replaceBlanks, queue2)!= 0){
+////        printf("Thread Creation Failed for Munch1");
+////        return 0;
+////    }
+////    if(pthread_create(&munch2, NULL, convertToLowerCase, queue3)!= 0){
+////        printf("Thread Creation Failed for Munch2");
+////        return 0;
+////    }
+//    EnqueueString(queue3, "abc");
+//    EnqueueString(queue3, "defg");
+//    EnqueueString(queue3, "hijk");
+//    EnqueueString(queue3, NULL);
+//    if(pthread_create(&writer, NULL, printOutput, (void *)queue3)){
+//        printf("Thread Creation Failed for Writer");
 //        return 0;
 //    }
-//    if(pthread_create(&munch1, NULL, replaceBlanks, queue2)!= 0){
-//        printf("Thread Creation Failed for Munch1");
-//        return 0;
-//    }
-//    if(pthread_create(&munch2, NULL, convertToLowerCase, queue3)!= 0){
-//        printf("Thread Creation Failed for Munch2");
-//        return 0;
-//    }
-    EnqueueString(queue3, "abc");
-    EnqueueString(queue3, "defg");
-    EnqueueString(queue3, "hijk");
-    EnqueueString(queue3, NULL);
-    if(pthread_create(&writer, NULL, printOutput, (void *)queue3)){
-        printf("Thread Creation Failed for Writer");
-        return 0;
-    }
-//    pthread_join(reader, (void **) &queue);
-//    pthread_join(munch1, (void **) &queue);
-//    pthread_join(munch2, (void **) &queue);
-
-    pthread_join(writer, NULL);
-
-//    EnqueueString(queue, "defg");
-//    EnqueueString(queue, "hijk");
-
-
-    PrintQueueStats(queue3);
-    return 0;
-}
+////    pthread_join(reader, (void **) &queue);
+////    pthread_join(munch1, (void **) &queue);
+////    pthread_join(munch2, (void **) &queue);
+//
+//    pthread_join(writer, NULL);
+//
+////    EnqueueString(queue, "defg");
+////    EnqueueString(queue, "hijk");
+//
+//
+//    PrintQueueStats(queue3);
+//    return 0;
+//}
--- reader.c
+++ reader.c
@@ -3,7 +3,38 @@
 //
 
 #include "reader.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
 
-void *readInput(void *arg){
+void *readInputTemp(void *arg){
 
+    Queue *queue = (Queue *) arg;
+    char *inputBuffer;
+    size_t bufferSize = 32;
+
+    inputBuffer = (char *)malloc(bufferSize * sizeof(char));
+    if( inputBuffer == NULL)
+    {
+        perror("Unable to allocate memory for reader input buffer");
+        exit(-1);
+    }
+
+    // Read the input file till you detect an EOF character
+    long long int M;
+    do {
+        getline(&inputBuffer,&bufferSize,stdin);
+        EnqueueString(queue, inputBuffer);
+        char *string = DequeueString(queue);
+        printf("%s\n", string);
+    } while(scanf("%lld", &M)!= EOF);
+
+    EnqueueString(queue, NULL);
+
+    return(0);
+}
+
+int main() {
+    Queue *queue = CreateStringQueue(10);
+    readInputTemp(queue);
 }
