92286ece03b6ee5d4e2c121481cad2aa56a4ebd2
anshu <anshu.iiita.10@gmail.com>
2018-10-18 18:39:21 -0500

    Adding Munch1 and Writer and main

--- munch2.c
+++ munch2.c
@@ -2,8 +2,42 @@
 // Created by Anshu on 10/14/18.
 //
 
+#include <string.h>
+#include <stdlib.h>
 #include "munch2.h"
+#include "struct_args.h"
+#include "queue.h"
+
 
 void *convertToLowerCase(void *arg){
+
+    // structure to hold both the queues
+    struct_args *args = (struct_args *) arg;
+
+    // Queue between Munch1 and Munch2
+    Queue *munch1ToMunch2 = args->q1;
+
+    // Queue between Munch2 and writer
+    Queue *munch2ToWriter = args->q2;
+
+    char *data = DequeueString(munch1ToMunch2);
+
+    // If you find a NULL, then terminate the thread
+    while (data != NULL){
+        int i = 0;
+        char *str = strdup(data);
+        while (str[i])
+        {
+            if (str[i] >= 97 && str[i] <= 122)
+                str[i] -= 32;
+            i++;
+        }
+        EnqueueString(munch2ToWriter, str);
+        data = DequeueString(munch1ToMunch2);
+    }
+
+    free(data);
+
+    pthread_exit(0);
 
 }
---
+++ struct_args.h
@@ -0,0 +1,15 @@
+//
+// Created by Anshu on 10/18/18.
+//
+
+#include "queue.h"
+
+#ifndef OS_HW2_STRUCT_ARGS_H
+#define OS_HW2_STRUCT_ARGS_H
+
+#endif //OS_HW2_STRUCT_ARGS_H
+
+typedef struct struct_args{
+    Queue *q1;
+    Queue *q2;
+}struct_args;
