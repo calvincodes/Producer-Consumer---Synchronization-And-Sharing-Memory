4cd16ba31a3a579b8f6fa9dc97a5ae0469c9615e
calvincodes <arpitjain1821@gmail.com>
2018-10-18 17:32:20 -0500

    Tested reader module. Added constants module.

---
+++ constants.h
@@ -0,0 +1,10 @@
+//
+// Created by Arpit Jain on 10/18/18.
+//
+
+#ifndef OS_HW2_CONSTANTS_H
+#define OS_HW2_CONSTANTS_H
+
+#endif //OS_HW2_CONSTANTS_H
+
+extern const unsigned int BUFFER_SIZE;
--- reader.c
+++ reader.c
@@ -6,35 +6,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "queue.h"
+#include "constants.h"
 
 void *readInputTemp(void *arg){
 
     Queue *queue = (Queue *) arg;
     char *inputBuffer;
-    size_t bufferSize = 32;
 
-    inputBuffer = (char *)malloc(bufferSize * sizeof(char));
+    inputBuffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
     if( inputBuffer == NULL)
     {
         perror("Unable to allocate memory for reader input buffer");
         exit(-1);
     }
 
-    // Read the input file till you detect an EOF character
-    long long int M;
+    int c;
+    int index = 0;
     do {
-        getline(&inputBuffer,&bufferSize,stdin);
-        EnqueueString(queue, inputBuffer);
-        char *string = DequeueString(queue);
-        printf("%s\n", string);
-    } while(scanf("%lld", &M)!= EOF);
+        c = getchar();
+        // Any processing is required only if encountered character is NOT EOF.
+        if (c != EOF) {
+            // If character read is not terminating this line and buffer is exhausted, throw error.
+            if (index >= BUFFER_SIZE && c != '\n') {
+                fprintf(stderr, "Line size exceeds buffer size %d", BUFFER_SIZE);
+                exit(-1);
+            }
 
-    EnqueueString(queue, NULL);
+            // Add to buffer if there is space available.
+            // Corner case here is, if your input string is of same length as buffer size and the newLine is
+            // the character at BUFFER_SIZE index. In this case, we will not throw error. Rather we have the
+            // entire line in our buffer and we will simply read the entire buffer everywhere, till be reach
+            // the end of buffer or we encounter a '\n' character.
+            if (index < BUFFER_SIZE) {
+                // Add current character to input buffer
+                inputBuffer[index++] = (char) c;
+            }
+
+            // Check if current character is the end of line for current line
+            if (c == '\n') {
+
+                // Enqueue the input buffer till now in the queue.
+                EnqueueString(queue, inputBuffer);
+
+                // TODO: Remove. These are for module testing purpose only.
+//                char *string = DequeueString(queue);
+//                printf("%s\n", string);
+
+                // Reset index, free up existing memory and allocate fresh memory for input buffer.
+                index = 0;
+                free(inputBuffer);
+                inputBuffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
+            }
+        }
+    } while(c != EOF);
 
     return(0);
 }
 
-int main() {
-    Queue *queue = CreateStringQueue(10);
-    readInputTemp(queue);
-}
+// TODO: Remove. This is for module testing purpose only.
+//int main() {
+//    Queue *queue = CreateStringQueue(10);
+//    readInputTemp(queue);
+//}
