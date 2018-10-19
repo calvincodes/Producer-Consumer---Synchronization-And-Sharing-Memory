22546a75a5bc02f074c2367e4b7cc4f28910b8c7
calvincodes <arpitjain1821@gmail.com>
2018-10-19 16:31:45 -0500

    Changing counter to long from int

--- writer.c
+++ writer.c
@@ -13,14 +13,14 @@ void* printOutput(void *arg){
     Queue *munch2ToWriter = (Queue *) arg;
     char *string = DequeueString(munch2ToWriter);
 
-    int i = 0;
+    long i = 0;
     while(string != NULL) {
         i++;
         printf("%s\n", string);
         free(string);
         string = DequeueString(munch2ToWriter);
     }
-    printf("TOTAL LINES PROCESSED: %d", i);
+    printf("TOTAL LINES PROCESSED: %ld", i);
 
     pthread_exit(0);
 }
